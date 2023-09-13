f80b323f2f05025f6e3e9e7b18ed9b2fd7eb5907
sylvan-li <76680992+sylvan-li@users.noreply.github.com>
2023-09-13 00:05:24 -0700

    Added js file

---
+++ website.js
@@ -0,0 +1,3 @@
+//Addition of another javascript for external file, greets the user and welcomes them to the site. //
+//hello world type program on an external js.file 
+alert("Thanks for visiting my site!");
